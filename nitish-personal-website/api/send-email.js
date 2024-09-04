311884584aba235f1af91f33135623a23fdb1cd9
Nitish Madabusi <nit.madabusi@gmail.com>
2024-09-03 22:56:46 -0400

    adding another fix

--- nitish-personal-website/api/send-email.js
+++ nitish-personal-website/api/send-email.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
     const serviceId = process.env.EMAILJS_SERVICE_ID;
     const templateId = process.env.EMAILJS_TEMPLATE_ID;
-    const userId = process.env.EMAILJS_USER_ID;
+    const publicKey = process.env.EMAILJS_PUBLIC_KEY; // Use the public API key here
 
     const templateParams = {
       from_name: name,
@@ -19,18 +19,16 @@ export default async function handler(req, res) {
       const response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', {
         service_id: serviceId,
         template_id: templateId,
-        user_id: userId,
+        user_id: publicKey, // Ensure you use the public key here
         template_params: templateParams,
       });
 
       if (response.status === 200) {
         res.status(200).json({ message: 'Email sent successfully' });
       } else {
-        console.error('Email sending failed', response.data);
-        res.status(500).json({ message: 'Email sending failed', error: response.data });
+        res.status(500).json({ message: 'Email sending failed', error: response });
       }
     } catch (error) {
-      console.error('Error:', error.message, error.response?.data);
       res.status(500).json({ message: 'Email sending failed', error: error.message });
     }
   } else {
