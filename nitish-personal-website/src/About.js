a7f36ee81028170a47e9570b6608cb435f5aa71f
Nitish Madabusi <nit.madabusi@gmail.com>
2024-09-04 00:21:27 -0400

    pushing new description

--- nitish-personal-website/src/About.js
+++ nitish-personal-website/src/About.js
@@ -12,7 +12,7 @@ const About = () => {
           </div>
           <div className="column right">
             <p className="intro">
-            Hello, I'm <span>Nitish</span>, currently in my fourth year at the University of Toronto, pursuing a degree in <span>Computer Science</span>. With a strong command of languages such as <span>C#, Go, Python, and SQL</span>, and experience in the health tech industry, I am committed to expanding my skills across various sectors. My enthusiasm for technology motivates me to continuously seek new challenges and learning opportunities.</p>
+            Hello, I'm <span>Nitish</span>, a fourth-year student at the University of Toronto, pursuing a degree in <span>Computer Science</span>. With a robust proficiency in languages like <span>C#, Go, Python, and SQL</span>, coupled with experience in the health tech industry, I am dedicated to broadening my expertise across diverse sectors. My passion for technology drives me to pursue new challenges and continuous learning opportunities.</p>
           </div>
         </div>
       </div>
